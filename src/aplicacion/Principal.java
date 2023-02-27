5a2cefc2a81f000ecf4b43f2c04ac3fb26d99f55
Álvaro <ainiguezdislaa@gmail.com>
2023-02-27 11:22:08 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,13 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.ArrayList;
+import java.util.List;
+import dominio.ej14_10;
+public class Principal {
+    public static void main(String[] args) {
+        List <String> al = new ArrayList<>();
+        al.add("Hola");
+        al.add("Mundo");
+        al.add("Java");
+        System.out.println(dominio.ej14_10.transformarAMinusculas(al));
+    }
 }
--- src/dominio/ej14_10.java
+++ src/dominio/ej14_10.java
@@ -1,2 +1,10 @@
-package dominio;public class ej14_10 {
+package dominio;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import  java.util.ArrayList;
+public class ej14_10 {
+    public static List<String> transformarAMinusculas(List<String> al){
+        return Stream.iterate(0,i->i+1).limit(al.size()).map(i->al.get(i).toLowerCase()).collect(Collectors.toCollection(ArrayList::new));
+    }
 }
